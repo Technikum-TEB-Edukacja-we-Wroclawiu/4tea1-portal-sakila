558bf2e7c7feb8a130512871ee21ad8fe1a8561c
Przemek Jedlikowski <przemek@jedlikowski.com>
2025-03-07 13:22:34 +0100

    Ćwiczenie do wykonania - podpowiedzi

---
+++ actor.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+- [ ] Sprawdź, czy jest ID - jeśli nie, to ->actors.php
+- [ ] Wyślij zapytanie do bazy danych, które pobierze jednego aktora
+- [ ] Jeśli nie przyszedł tylko jeden aktor z bazy, to ->actors.php
+- [ ] Pobierz dane o aktorze
+- [ ] Chwilowo zakomentuj część o obsadzie, później przerobisz
+- [ ] Wyświetl podstawowe informacje o aktorze w h1: imię i nazwisko
+- [ ] Ogarnij pobranie z bazy danych id i tytułów filmów, w których wyświetlany aktor brał udział
+- [ ] Wyświetl w formie listy `ul`
+*/
